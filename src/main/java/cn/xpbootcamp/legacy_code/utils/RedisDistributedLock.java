af010a31fe78baa0e9fa9f52676868f39067af55
Fu Yi <yifu@thoughtworks.com>
2020-03-12 14:23:09 +0800

    remove comments.

--- src/main/java/cn/xpbootcamp/legacy_code/utils/RedisDistributedLock.java
+++ src/main/java/cn/xpbootcamp/legacy_code/utils/RedisDistributedLock.java
@@ -1,6 +1,7 @@
 package cn.xpbootcamp.legacy_code.utils;
 
 public class RedisDistributedLock {
+
     private static final RedisDistributedLock INSTANCE = new RedisDistributedLock();
 
     public static RedisDistributedLock getSingletonInstance() {
@@ -8,12 +9,10 @@ public class RedisDistributedLock {
     }
 
     public boolean lock(String transactionId) {
-        // Here is connecting to redis server, please do not invoke directly
         throw new RuntimeException("Redis server is connecting......");
     }
 
     public void unlock(String transactionId) {
-        // Here is connecting to redis server, please do not invoke directly
         throw new RuntimeException("Redis server is connecting......");
     }
 }
